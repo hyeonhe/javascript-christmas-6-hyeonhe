51916bb986b97074eba34d0dccef03bf48ad6b25
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2023-11-12 20:55:58 +0900

    feat: 타이틀 상수 추가

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,6 +6,12 @@ const INPUT_MESSAGE = Object.freeze({
 const OUTPUT_MESSAGE = Object.freeze({
   greetingMessage: "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.",
   orderMenu: "<주문 메뉴>",
+  totalAmountBeforeDiscount: "<할인 전 총주문 금액>",
+  giftMenu: "<증정 메뉴>",
+  discount: "<혜택 내역>",
+  totalDiscountAmout: "<총혜택 금액>",
+  totalAmount: "<할인 후 예상 결제 금액>",
+  badge: "<12월 이벤트 배지>",
   appetizer:
     "<애피타이저>\n양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)",
   main: "<메인>\n티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)",
